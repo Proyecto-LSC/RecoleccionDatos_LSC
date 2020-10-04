1c8678dcbe74e08193ac20ea57411303289d544f
Edward Pinzon <edwardpinzon13@gmail.com>
2020-10-04 00:25:37 -0500

    templates

--- static/js/video2.js
+++ static/js/video2.js
@@ -1,72 +1,95 @@
-const  record = document.querySelector("#btnComenzarGrabacion");
-const  stop  = document.querySelector("#btnDetenerGrabacion");
-
+const record = document.querySelector("#btnComenzarGrabacion");
+const stop = document.querySelector("#btnDetenerGrabacion");
+const $duracion = document.querySelector("#duracion");
 var video = document.querySelector("#video");
 var canvas = document.querySelector("#canvas");
 
-        var constraints = { audio: false,  video: {width: 1280 ,   height: 720}};
-        let chunks = [];
-        
-        navigator.mediaDevices.getUserMedia(constraints)
-        .then(function(mediaStream){
-            video.srcObject = mediaStream;
-            video.play();  
-            var mediaRecorder = new MediaRecorder(mediaStream , {mimeType: "video/webm; codecs=vp9"});
-            record.onclick = function (){
-                console.log("pi ti ");
-                mediaRecorder.start();
-                console.log("hola");
-                console.log(mediaRecorder.state);
-                console.log("vhao");
-                stop.disabled = false;
-                record.disabled = true;
-            }
-            
-            mediaRecorder.ondataavailable= function(e){
-                chunks.push(e.data);
-            }
-            stop.onclick = function (){
-                mediaRecorder.stop();
-                console.log(mediaRecorder.state);
-                stop.disabled = true;
-                record.disabled = false;
-            } 
 
-            
+const segundosATiempo = numeroDeSegundos => {
+    let horas = Math.floor(numeroDeSegundos / 60 / 60);
+    numeroDeSegundos -= horas * 60 * 60;
+    let minutos = Math.floor(numeroDeSegundos / 60);
+    numeroDeSegundos -= minutos * 60;
+    numeroDeSegundos = parseInt(numeroDeSegundos);
+    if (horas < 10) horas = "0" + horas;
+    if (minutos < 10) minutos = "0" + minutos;
+    if (numeroDeSegundos < 10) numeroDeSegundos = "0" + numeroDeSegundos;
 
-            mediaRecorder.onstop = function(e){
-                const blobVideo = new Blob(chunks, {type: "video/webm"} );
-                
-                // Crear una URL o enlace para descargar
-                const urlParaDescargar = URL.createObjectURL(blobVideo);
-                
-                // Crear un elemento <a> invisible para descargar el audio
-                let a = document.createElement("a");
-                document.body.appendChild(a);
-                a.style = "display: none";
-                a.href = urlParaDescargar;
-                a.download = "video.webm";
-                // Hacer click en el enlace
-                a.click();
-                // Y remover el objeto
-                window.URL.revokeObjectURL(urlParaDescargar);
+    return `${horas}:${minutos}:${numeroDeSegundos}`;
+};
+var constraints = { audio: false, video: { width: 1280, height: 720 } };
+let chunks = [];
+let tiempoInicio, idIntervalo;
+const refrescar = () => {
+    $duracion.textContent = segundosATiempo((Date.now() - tiempoInicio) / 1000);
+}
+const comenzarAContar = () => {
+    tiempoInicio = Date.now();
+    idIntervalo = setInterval(refrescar, 500);
+};
 
-                // Crear una URL o enlace para descargar
-                    
-            }
+navigator.mediaDevices.getUserMedia(constraints)
+    .then(function (mediaStream) {
+        video.srcObject = mediaStream;
+        video.play();
+        var mediaRecorder = new MediaRecorder(mediaStream, { mimeType: "video/webm; codecs=vp9" });
+        record.onclick = function () {
+            console.log("pi ti ");
+            mediaRecorder.start();
+            comenzarAContar();
+            console.log("hola");
+            console.log(mediaRecorder.state);
+            console.log("vhao");
+            stop.disabled = false;
+            record.disabled = true;
+        }
 
-
-        }).catch(
-            function(err){
-                console.log(err.name + ": " + err.message);});
-    
-
-    
-                
+        mediaRecorder.ondataavailable = function (e) {
+            chunks.push(e.data);
+        }
+        stop.onclick = function () {
+            mediaRecorder.stop();
+            console.log(mediaRecorder.state);
+            stop.disabled = true;
+            record.disabled = false;
+        }
 
 
 
-        
+        mediaRecorder.onstop = function (e) {
+            const blobVideo = new Blob(chunks, { type: "video/webm" });
+            chunks=[];
+            // Crear una URL o enlace para descargar
+            const urlParaDescargar = URL.createObjectURL(blobVideo);
+
+            // Crear un elemento <a> invisible para descargar el audio
+            let a = document.createElement("a");
+            document.body.appendChild(a);
+            a.style = "display: none";
+            a.href = urlParaDescargar;
+            a.download = "video.webm";
+            // Hacer click en el enlace
+            a.click();
+            // Y remover el objeto
+            window.URL.revokeObjectURL(urlParaDescargar);
+
+            // Crear una URL o enlace para descargar
+
+        }
+
+
+    }).catch(
+        function (err) {
+            console.log(err.name + ": " + err.message);
+        });
+
+
+
+
+
+
+
+
 
 
 
