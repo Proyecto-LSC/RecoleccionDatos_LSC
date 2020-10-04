da44b59917a1bd2a35f6b95f46b164a161d55e01
Edward Pinzon <edwardpinzon13@gmail.com>
2020-10-04 09:56:36 -0500

    carouselImages

---
+++ static/js/carousel.js
@@ -0,0 +1,20 @@
+const imagecarousel = document.querySelector("#imagecarousel");
+
+
+/*
+function enviarImagenes(){
+    for ( var i = i ;i<10; i++){
+        setTimeout(CargarImagenes(i),10000);
+    }
+    
+}
+ 
+
+function CargarImagenes(i){
+    
+        console.log(i);
+        imagecarousel.src = "static/img/carouselhands/secuencia1/pose" + i+".jpg";
+        console.log("hola sss");
+}
+
+enviarImagenes()*/
