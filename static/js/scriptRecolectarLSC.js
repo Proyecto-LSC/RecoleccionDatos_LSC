94e26791e45f6aa21214364ed16d5774880d894a
Oscar Rodriguez <oscarstep.123@gmail.com>
2020-10-04 03:03:32 -0500

    Se añadio archivo javascript de recolectarLSC.html

---
+++ static/js/scriptRecolectarLSC.js
@@ -0,0 +1,7 @@
+var video = document.querySelector(".video-element");
+var record = document.getElementById("record");
+var stop = document.getElementById("stop");
+var letra_actual = document.getElementById("letra_actual");
+var letra_next = document.getElementById("letra_next");
+const canvas = document.querySelector('#canvas');
+const ctx = canvas.getContext('2d');
