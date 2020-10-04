4d25181df9471be351c5a3d1163e970cd523b21c
Oscar Rodriguez <oscarstep.123@gmail.com>
2020-10-04 03:29:55 -0500

    Permiso para activar la camara

--- static/js/scriptRecolectarLSC.js
+++ static/js/scriptRecolectarLSC.js
@@ -5,3 +5,20 @@ var letra_actual = document.getElementById("letra_actual");
 var letra_next = document.getElementById("letra_next");
 const canvas = document.querySelector('#canvas');
 const ctx = canvas.getContext('2d');
+
+if (navigator.mediaDevices.getUserMedia) {
+
+    navigator.mediaDevices.getUserMedia({ video: true })
+        .then(function (stream) {
+            video.srcObject = stream;
+    })
+
+    .catch(function (err0r) {
+      console.log("Something went wrong!");
+    });
+    //es un evento en donde carga la infomación del video
+    video.addEventListener('loadeddata', ()=>{
+        canvas.width = video.videoWidth;
+        canvas.height = video.videoHeight;
+    })
+}
