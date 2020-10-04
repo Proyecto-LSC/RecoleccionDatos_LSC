4f4e913afdacd6ea63c1e54adec475173f3a4117
Oscar Rodriguez <oscarstep.123@gmail.com>
2020-10-04 04:00:15 -0500

    Funcionalidad para grabar video por 15 segundos

--- static/js/scriptRecolectarLSC.js
+++ static/js/scriptRecolectarLSC.js
@@ -7,10 +7,28 @@ const canvas = document.querySelector('#canvas');
 const ctx = canvas.getContext('2d');
 
 if (navigator.mediaDevices.getUserMedia) {
+    var recordedChunks = [];
 
     navigator.mediaDevices.getUserMedia({ video: true })
         .then(function (stream) {
             video.srcObject = stream;
+            var mediaRecorder = new MediaRecorder(stream);
+
+            record.onclick = function startRecording() {
+                var numSec = 15;
+				recordedChunks = [];
+				mediaRecorder.start();
+				document.getElementById("recordingIcon").style.display = 'inline-block';
+				document.getElementById("textGrabando").style.display = 'inline-block';
+				console.log(mediaRecorder.state);
+				console.log("recorder started");
+				setTimeout(event => {
+					console.log("stopping");
+					document.getElementById("recordingIcon").style.display = 'none';
+					document.getElementById("textGrabando").style.display = 'none';
+					mediaRecorder.stop();
+				}, (numSec*1000));
+            }
     })
 
     .catch(function (err0r) {
