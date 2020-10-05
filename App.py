c36e332a7af27d50ed07b06228c14b6d3b14c3b2
Oscar Rodriguez <oscarstep.123@gmail.com>
2020-10-04 19:09:48 -0500

    Crear frames de los videos de LSC

--- App.py
+++ App.py
@@ -1,4 +1,4 @@
-import os, glob
+import os, glob, cv2
 from flask import Flask, render_template, request, Response
 from pathlib import Path
 
@@ -29,7 +29,31 @@ def getVideo():
             filename = str(filepath.parents[0].name) + "_" + str(lastVideoNum) + ".mp4"
             video.save(fullpath + filename)
             filepath = Path(fullpath + filename)
+    
+    video2frames(filepath)
     return Response("success")
+
+def video2frames(filepath):
+    vidcap = cv2.VideoCapture(str(filepath))
+    list_of_files = glob.glob(str(filepath.parents[0]) + '/frames/*')
+    success = True
+    count = 0
+    imgNum = 0
+    
+    while success:
+        success,image = vidcap.read()
+        if not list_of_files:
+            if count%(0.6*30) == 0 :
+                imgNum +=1
+                cv2.imwrite((str(filepath.parents[0]) + "/frames/" + str(filepath.parents[0].name) + "_%d.jpg") % imgNum, image) # save frame as JPEG file
+        else:
+            list_of_files = glob.glob(str(filepath.parents[0]) + '/frames/*')
+            latest_file = max(list_of_files, key=os.path.getctime)
+            tempPath = Path(latest_file)
+            lastImgNum = int(tempPath.stem.split("_", 1) [1]) + 1
+            if count%(0.6*30) == 0 :
+                cv2.imwrite((str(filepath.parents[0]) + "/frames/" + str(filepath.parents[0].name) + "_%d.jpg") % lastImgNum, image) # save frame as JPEG file
+        count += 1
 
 @app.route('/recolectorManos')
 def HandsRecollector():
