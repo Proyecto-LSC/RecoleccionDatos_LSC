664aa8f28a0bef4056233e770cebff92199417c8
Edward Pinzon <edwardpinzon13@gmail.com>
2020-10-13 15:10:30 -0500

    imagenes y tiempo

--- static/js/video2.js
+++ static/js/video2.js
@@ -3,7 +3,7 @@ const stop = document.querySelector("#btnDetenerGrabacion");
 const $duracion = document.querySelector("#duracion");
 var video = document.querySelector("#video");
 var canvas = document.querySelector("#canvas");
-
+var listo = document.querySelector("#listo")
 
 const segundosATiempo = numeroDeSegundos => {
     let horas = Math.floor(numeroDeSegundos / 60 / 60);
@@ -41,6 +41,7 @@ navigator.mediaDevices.getUserMedia(constraints)
         video.play();
         var mediaRecorder = new MediaRecorder(mediaStream, { mimeType: "video/webm; codecs=vp9" });
         record.onclick = function () {
+            var numSec=112;
             console.log("pi ti ");
             mediaRecorder.start();
             $('.carousel').carousel(0,{
@@ -53,6 +54,13 @@ navigator.mediaDevices.getUserMedia(constraints)
             console.log("vhao");
             stop.disabled = false;
             record.disabled = true;
+            setTimeout(event => {
+                console.log("stopping");
+                mediaRecorder.stop();
+                detenerConteo();
+                stop.disabled = true;
+                record.disabled = false;
+            }, (numSec*1000));
         }
 
         mediaRecorder.ondataavailable = function (e) {
@@ -67,32 +75,11 @@ navigator.mediaDevices.getUserMedia(constraints)
             record.disabled = false;
         }
 
-
-
-
-
         mediaRecorder.onstop = function (e) {
             const blobVideo = new Blob(chunks, { type: "video/mp4" });
             chunks=[];
             
             sendToFlask(blobVideo)
-            /*
-            // Crear una URL o enlace para descargar
-            const urlParaDescargar = URL.createObjectURL(blobVideo);
-
-            // Crear un elemento <a> invisible para descargar el audio
-            let a = document.createElement("a");
-            document.body.appendChild(a);
-            a.style = "display: none";
-            a.href = urlParaDescargar;
-            a.download = "video.webm";
-            // Hacer click en el enlace
-            a.click();
-            // Y remover el objeto
-            window.URL.revokeObjectURL(urlParaDescargar);
-
-            // Crear una URL o enlace para descargar*/
-
         }
     }).catch(
         function (err) {
@@ -116,6 +103,25 @@ navigator.mediaDevices.getUserMedia(constraints)
             });
         }
 
+
+        function muestra_oculta(){
+                var el = document.getElementById("imagenesCarou");
+                var ela = document.getElementById("grabar"); //se define la variable "el" igual a nuestro div
+                el.style.display = (el.style.display == 'none') ? 'block' : 'none'; //damos un atributo display:none que oculta el div
+                ela.style.display = (ela.style.display=='none') ? 'block' : 'none';
+            }
+
+        listo.onclick = function (){
+            var gravar = document.getElementById("grabar");
+            gravar.style.display = (gravar.style.display='none') ? 'block' : 'none';
+            var ela = document.getElementById("imagenesCarou");
+            ela.style.display = (ela.style.display=='none') ? 'block' : 'none';
+            var el = document.getElementById("instrucciones");
+            el.style.display = (el.style.display == 'none') ? 'block' : 'none'; //damos un atributo display:none que oculta el div
+        }
+
+
+        muestra_oculta();
 
 
 
